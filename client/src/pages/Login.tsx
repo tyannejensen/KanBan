dedb112c730c8611a2d454aa697912b754e83b58
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2025-02-02 21:19:07 -0700

    updated error message

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,19 +1,22 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 import { login } from "../api/authAPI";
 
 const Login = () => {
   const [loginData, setLoginData] = useState({
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   });
+  const [error, setError] = useState("");
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setLoginData({
       ...loginData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -23,33 +26,34 @@ const Login = () => {
       const data = await login(loginData);
       Auth.login(data.token);
     } catch (err) {
-      console.error('Failed to login', err);
+      console.error("Failed to login", err);
+      setError("Failed to login");
     }
   };
 
   return (
-    <div className='container'>
-      <form className='form' onSubmit={handleSubmit}>
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
         <h1>Login</h1>
-        <label >Username</label>
-        <input 
-          type='text'
-          name='username'
-          value={loginData.username || ''}
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          value={loginData.username || ""}
           onChange={handleChange}
         />
-      <label>Password</label>
-        <input 
-          type='password'
-          name='password'
-          value={loginData.password || ''}
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={loginData.password || ""}
           onChange={handleChange}
         />
-        <button type='submit'>Submit Form</button>
+        <button type="submit">Submit Form</button>
+        {error && <p>{error}</p>}
       </form>
     </div>
-    
-  )
+  );
 };
 
 export default Login;
